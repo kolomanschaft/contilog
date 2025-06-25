4c01587a6c4efeb31e15826d099d6e6608721521
Martin Blum <martin@blum.team>
2025-06-25 08:17:26 +0200

    Initial topics view

---
+++ Models/Topic.cs
@@ -0,0 +1,11 @@
+namespace Contilog.Models;
+
+public class Topic
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public string Author { get; set; } = string.Empty;
+    public bool IsOpen { get; set; }
+}
