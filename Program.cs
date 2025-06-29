4bfe4d9a4f59bfc85ff0aa086931489f319c8d45
Martin Blum <martin@blum.team>
2025-06-29 13:46:17 +0200

    Ger rod of the template pages.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Contilog.Data;
 using Contilog.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 // Register repositories
 builder.Services.AddSingleton<ITopicRepository, TopicRepository>();
